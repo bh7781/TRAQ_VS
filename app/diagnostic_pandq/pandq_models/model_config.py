a54ac4d0eb4ed9dc1ecc32d50c5daf4e33cec87b
Chinmay Shelke <chinmay.s077@gmail.com>
2025-02-22 18:04:58 +0530

    added entry point for model creation

--- app/diagnostic_pandq/pandq_models/model_config.py
+++ app/diagnostic_pandq/pandq_models/model_config.py
@@ -1,67 +1,48 @@
+# model_config.py
 import os
-from common import constants
-
 from common.utility import adjust_path_for_os
 
-
-def get_model_configs(env):
-    # Define base paths for different environments
-    pandq_output_base_path = f'/v/region/na/appl/gtr/ttro_it_diagnostic/data/{env}/pandq_daily'
-    # For local testing
-    # pandq_output_base_path = rf'C:\Users\{os.getlogin()}\Morgan Stanley\Tech & TRAQ Automation - Diagnostic Testing\pandq_daily'
-    pandq_output_base_path = adjust_path_for_os(pandq_output_base_path)
-
-    # Define base paths for different environments
-    pandq_config_base_path = f'/v/region/na/appl/gtr/ttro_it_diagnostic/data/{env}/pandq_config'
-    # For local testing
-    # pandq_config_base_path = rf'C:\Users\{os.getlogin()}\Morgan Stanley\Tech & TRAQ Automation - Diagnostic Testing\pandq_config'
-    pandq_config_base_path = adjust_path_for_os(pandq_config_base_path)
-
-    # Construct output file paths for each regime and asset class
-    configs = {
-        constants.JFSA: {
-            'filesdqconfig_path_qa': os.path.join(pandq_output_base_path, 'jfsa2'),
-            'filesdqconfig_path_prod': os.path.join(pandq_output_base_path, 'jfsa2'),
-            'output_path': os.path.join(pandq_config_base_path, 'jfsa2', 'metadata'),
-            'model_version': '14.10.2024',
-            'model_id': 'TBD',
-            'model_name': 'TBD_JFSA',
-            'input_files_folder_path': os.path.join(pandq_output_base_path, 'jfsa2'),
-            'column_mapping': os.path.join(pandq_config_base_path, 'jfsa2', 'column_mapping', 'jfsa_diagnostic_field_name_mapping.json'),
-        },
-
-        constants.ASIC: {
-            'filesdqconfig_path_qa': os.path.join(pandq_output_base_path, 'asic2'),
-            'filesdqconfig_path_prod': os.path.join(pandq_output_base_path, 'asic2'),
-            'output_path': os.path.join(pandq_config_base_path, 'asic2', 'metadata'),
-            'model_version': '14.10.2024',
-            'model_id': 'TBD',
-            'model_name': 'TBD_ASIC',
-            'input_files_folder_path': os.path.join(pandq_output_base_path, 'asic2'),
-            'column_mapping': os.path.join(pandq_config_base_path, 'asic2', 'column_mapping', 'asic_diagnostic_field_name_mapping.json'),
-        },
-
-        constants.MAS: {
-            'filesdqconfig_path_qa': os.path.join(pandq_output_base_path, 'mas2'),
-            'filesdqconfig_path_prod': os.path.join(pandq_output_base_path, 'mas2'),
-            'output_path': os.path.join(pandq_config_base_path, 'mas2', 'metadata'),
-            'model_version': '14.10.2024',
-            'model_id': 'TBD',
-            'model_name': 'TBD_MAS',
-            'input_files_folder_path': os.path.join(pandq_output_base_path, 'mas2'),
-            'column_mapping': os.path.join(pandq_config_base_path, 'mas2', 'column_mapping', 'mas_diagnostic_field_name_mapping.json'),
-        },
-
-        constants.EMIR_REFIT: {
-            'filesdqconfig_path_qa': os.path.join(pandq_output_base_path, 'emir_refit'),
-            'filesdqconfig_path_prod': os.path.join(pandq_output_base_path, 'emir_refit'),
-            'output_path': os.path.join(pandq_config_base_path, 'emir_refit', 'metadata'),
-            'model_version': '14.10.2024',
-            'model_id': 'TBD',
-            'model_name': 'TBD_MAS',
-            'input_files_folder_path': os.path.join(pandq_output_base_path, 'emir_refit'),
-            'column_mapping': os.path.join(pandq_config_base_path, 'emir_refit', 'column_mapping', 'emir_refit_diagnostic_field_name_mapping.json'),
-        }
+# A simplified configuration dictionary for each regime.
+# (You can update the paths as needed.)
+MODEL_CONFIGS = {
+    'JFSA': {
+        'filesdqconfig_path_qa': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/jfsa2'),
+        'filesdqconfig_path_prod': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/prod/pandq_daily/jfsa2'),
+        'output_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/jfsa2/metadata'),
+        'model_version': '14.10.2024',
+        'model_id': 'TBD',
+        'model_name': 'TBD_JFSA',
+        'input_files_folder_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/jfsa2'),
+        'column_mapping': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/jfsa2/column_mapping/jfsa_diagnostic_field_name_mapping.json'),
+    },
+    'ASIC': {
+        'filesdqconfig_path_qa': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/asic2'),
+        'filesdqconfig_path_prod': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/prod/pandq_daily/asic2'),
+        'output_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/asic2/metadata'),
+        'model_version': '14.10.2024',
+        'model_id': 'TBD',
+        'model_name': 'TBD_ASIC',
+        'input_files_folder_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/asic2'),
+        'column_mapping': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/asic2/column_mapping/asic_diagnostic_field_name_mapping.json'),
+    },
+    'MAS': {
+        'filesdqconfig_path_qa': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/mas2'),
+        'filesdqconfig_path_prod': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/prod/pandq_daily/mas2'),
+        'output_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/mas2/metadata'),
+        'model_version': '14.10.2024',
+        'model_id': 'TBD',
+        'model_name': 'TBD_MAS',
+        'input_files_folder_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/mas2'),
+        'column_mapping': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/mas2/column_mapping/mas_diagnostic_field_name_mapping.json'),
+    },
+    'EMIR_REFIT': {
+        'filesdqconfig_path_qa': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/emir_refit'),
+        'filesdqconfig_path_prod': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/prod/pandq_daily/emir_refit'),
+        'output_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/emir_refit/metadata'),
+        'model_version': '14.10.2024',
+        'model_id': 'TBD',
+        'model_name': 'TBD_EMIR_REFIT',
+        'input_files_folder_path': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_daily/emir_refit'),
+        'column_mapping': adjust_path_for_os('/v/region/na/appl/gtr/ttro_it_diagnostic/data/qa/pandq_config/emir_refit/column_mapping/emir_refit_diagnostic_field_name_mapping.json'),
     }
-
-    return configs
+}
